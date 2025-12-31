79a5acfd1f60a5ba005f1d55c8e0b8246508321e
harsh091234 <hsharma4090@gmail.com>
2025-12-31 18:48:52 +0530

    .

--- server/server.js
+++ server/server.js
@@ -4,18 +4,31 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/authRoute.js"
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from 'path';
 
 dotenv.config();
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
+
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
+const __dirname = path.resolve()
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("hello"));
 
 app.use("/api/auth", authRoutes)
+
+
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"))
+    })
+}
+
+
 
 app.listen(PORT, () => {
     connectDB();
